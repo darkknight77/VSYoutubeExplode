cc0d01c0316a1ff94fd3774bb875e5c8ffc9cca2
darkknight77 <shaiklatheefmrf98@gmail.com>
2020-03-27 01:05:23 +0530

    Added: Progress of download on the UI

--- YoutubeExplodeAPIDemo/MainPage.xaml.cs
+++ YoutubeExplodeAPIDemo/MainPage.xaml.cs
@@ -4,8 +4,11 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Networking.BackgroundTransfer;
 using Windows.Storage;
 using Windows.Storage.Streams;
 using Windows.UI.Xaml;
@@ -27,6 +30,11 @@ namespace YoutubeExplodeAPIDemo
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        DownloadOperation downloadOperation;
+        CancellationTokenSource cancellationToken;
+        double progress;
+
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -43,12 +51,24 @@ namespace YoutubeExplodeAPIDemo
             var video = await client.GetVideoAsync(id);
 
             var title = video.Title; // "Infected Mushroom - Spitfire [Monstercat Release]"
+            StatusText.Text = String.Format("{0} ", title);
             var author = video.Author; // "Monstercat"
             var duration = video.Duration; // 00:07:14
 
-            propText.Text = $"        id: {id}    client: {client}   title: {title}      author: {author}      duration: {duration} ";
+            StatusText.Text = $"        id: {id}    client: {client}   title: {title}      author: {author}      duration: {duration} ";
 
             var streamInfoSet = await client.GetVideoMediaStreamInfosAsync(id);
+
+            // ...or highest bitrate audio stream
+            // var streamInfo = streamInfoSet.Audio.WithHighestBitrate();
+
+            // ...or highest quality & highest framerate MP4 video stream
+            // var streamInfo = streamInfoSet.Video
+            //    .Where(s => s.Container == Container.Mp4)
+            //    .OrderByDescending(s => s.VideoQuality)
+            //    .ThenByDescending(s => s.Framerate)
+            //    .First();
+
 
             var streamInfo = streamInfoSet.Muxed.WithHighestVideoQuality();
 
@@ -57,17 +77,51 @@ namespace YoutubeExplodeAPIDemo
             Debug.WriteLine("Trying to download");
 
             //Windows.Storage.StorageFolder folder = await ApplicationData.Current.LocalFolder;
+
+            // StorageFile file = await DownloadsFolder.CreateFileAsync($"file.{ext}");
+
+            var file = await KnownFolders.VideosLibrary.OpenStreamForWriteAsync($"file.{ext}", CreationCollisionOption.GenerateUniqueName);
            
-           // StorageFile file = await DownloadsFolder.CreateFileAsync($"file.{ext}");
+            Progress = 0;
 
-             var file = await KnownFolders.VideosLibrary.OpenStreamForWriteAsync($"file.{ext}", CreationCollisionOption.GenerateUniqueName);
+            var progressHandler = new Progress<double>(p => 
+            { 
+                Progress = p;
+                
+                StatusText1.Text = String.Format("{0}% of {1}% complete.", Convert.ToInt32(Math.Floor(p * 100)), 100);
+                Debug.WriteLine(progress);
+            });
+
+             
+            StatusText.Text = this.progress.ToString();
+
+            
             //IRandomAccessStream fileStream = await file.OpenAsync(FileAccessMode.Read);
-            await client.DownloadMediaStreamAsync(streamInfo, file);
-          //  IRandomAccessStream fileStream = await file.OpenAsync(FileAccessMode.Read);
-
+            try
+            {
+                await client.DownloadMediaStreamAsync(streamInfo, file, progressHandler);
+            }
+            catch (Exception ex) {
+                
+                Debug.WriteLine(ex.ToString());
+                StatusText1.Text = "Cannot download";
+            }
+            //  IRandomAccessStream fileStream = await file.OpenAsync(FileAccessMode.Read);
+           
+        
         }
 
-
+        
+        public double Progress
+        {
+            get
+            {   return  this.progress;
+         }
+            set
+            {
+                this.progress = value;
+            }
+        }
 
     }
 }
